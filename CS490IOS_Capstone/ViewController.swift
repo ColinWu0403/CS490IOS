944aaa4501aec563f66c09aa8e8611ca6da8607a
Colin Wu <colinwu0403@gmail.com>
2025-03-25 16:38:12 -0400

    Initial Commit

---
+++ CS490IOS_Capstone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS490IOS_Capstone
+//
+//  Created by colin wu on 25/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
