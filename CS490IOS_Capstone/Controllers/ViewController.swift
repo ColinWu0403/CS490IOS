d3cba77ee4b4d8e423fa46602e41b01dd0a4991f
Colin Wu <colinwu0403@gmail.com>
2025-03-28 14:49:12 -0400

    Add tests and example data upload

--- CS490IOS_Capstone/Controllers/ViewController.swift
+++ CS490IOS_Capstone/Controllers/ViewController.swift
@@ -6,12 +6,46 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create a test review instance
+        let testReview = Review(
+            restaurantName: "Test Restaurant",
+            cuisine: "Italian",
+            location: "123 Test Street",
+            score: 4.5,
+            reviewText: "Great food and service!",
+            photoURL: "http://example.com/photo.jpg"
+        )
+        
+        // Call the addReview function with the test review
+        addReview(testReview)
+    }
+
+    
+    func addReview(_ review: Review) {
+        var ref: DocumentReference? = nil
+        ref = db.collection("Reviews").addDocument(data: [
+            "restaurantName": review.restaurantName,
+            "cuisine": review.cuisine,
+            "location": review.location,
+            "score": review.score,
+            "reviewText": review.reviewText,
+            "photoURL": review.photoURL
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
     }
 
 
