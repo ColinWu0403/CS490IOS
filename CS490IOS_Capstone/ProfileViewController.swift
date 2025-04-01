92f84ebc779f9de0c4dfbb3caddfe3c06a388b34
aysusaglam <esaglam@purdue.edu>
2025-04-01 17:40:40 -0400

    View contollers fix - now recognized by storyboard

--- CS490IOS_Capstone/ProfileViewController.swift
+++ CS490IOS_Capstone/ProfileViewController.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var usernameLabel: UILabel!
-    @IBOutlet weak var profilePicture: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Dummy data
-        usernameLabel.text = "foodie7"
-        profilePicture.image = UIImage(named: "profilePlaceholder")
+
+        // Do any additional setup after loading the view.
     }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
