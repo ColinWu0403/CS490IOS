5e142fe6cd8fc5bb9cc999bde9b21fb2d44a8e87
aysusaglam <esaglam@purdue.edu>
2025-04-01 17:20:06 -0400

    Basic tab bar controller with view controller files.

---
+++ CS490IOS_Capstone/AddReviewViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddReviewViewController.swift
+//  CS490IOS_Capstone
+//
+//  Created by Aysu Saglam on 4/1/25.
+//
+import UIKit
+
+class AddReviewViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
